c6e511897316feb699530705307796bc35cef6f6
Edson <Edson@DESKTOP-D0BM5A4>
2022-09-21 21:31:13 -0300

    Adicionar arquivos de projeto.

---
+++ DotNetCoreConsoleLogging/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace DotNetCoreConsoleLogging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation("Esta é uma mensagem log - Diretório atual {0}", Directory.GetCurrentDirectory());
+        }
+    }
+}
